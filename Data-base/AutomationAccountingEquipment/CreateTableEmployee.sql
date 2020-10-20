a8365ed48c5ea8e9120860d8c8929103ec376ebf
Entegrio-tech <72520235+Entegrio-tech@users.noreply.github.com>
2020-10-20 21:03:44 +0300

    Revert "Обновление таблици Employee"
    This reverts commit f6adbbc4c42e5176d0979bb9c6b212b781845ab9.

--- Data-base/AutomationAccountingEquipment/CreateTableEmployee.sql
+++ Data-base/AutomationAccountingEquipment/CreateTableEmployee.sql
@@ -5,6 +5,7 @@ CREATE TABLE Employee
   id SMALLINT IDENTITY,
   FirstName NVARCHAR(20),
   LastName NVARCHAR(20),
+  Phone VARCHAR(20),
   Login VARCHAR(20),
   Password VARBINARY(30),
   Privilage bit,
