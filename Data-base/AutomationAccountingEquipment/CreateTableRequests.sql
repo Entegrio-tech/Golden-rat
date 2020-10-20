c88dcd6feeff2d8841be320479ddab41683bac53
Entegrio-tech <72520235+Entegrio-tech@users.noreply.github.com>
2020-10-20 21:33:21 +0300

    Новое поле HostID в таблице Requests
    Добалено новое поле HostID (внешний ключ) в таблице Requests

--- Data-base/AutomationAccountingEquipment/CreateTableRequests.sql
+++ Data-base/AutomationAccountingEquipment/CreateTableRequests.sql
@@ -6,6 +6,7 @@ CREATE TABLE Requests
 	EmployeeID SMALLINT REFERENCES Employee (id),
 	EquipmentID SMALLINT REFERENCES Equipment (ID),
 	Text NVARCHAR(500),
+    HostID SMALLINT REFERENCES Employee (id),
 	RequestDate DATE
 );
 
