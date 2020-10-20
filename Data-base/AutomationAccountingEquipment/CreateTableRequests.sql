cabcb46229c931fdae3c954822a276a97d210bd4
manswisskinfe <62439500+manswisskinfe@users.noreply.github.com>
2020-10-20 19:24:53 +0300

    Update CreateTableRequests.sql

--- Data-base/AutomationAccountingEquipment/CreateTableRequests.sql
+++ Data-base/AutomationAccountingEquipment/CreateTableRequests.sql
@@ -6,8 +6,6 @@ CREATE TABLE Requests
 	EmployeeID SMALLINT REFERENCES Employee (id),
 	EquipmentID SMALLINT REFERENCES Equipment (ID),
 	Text NVARCHAR(500),
-	RequestDate DATE,
-	Inventory_number NVARCHAR(30),
-	Components NVARCHAR(1000),
+	RequestDate DATE
 );
 
