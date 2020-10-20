1a9182a3fd681a640e198dddbe50eb0a062d92b2
Entegrio-tech <72520235+Entegrio-tech@users.noreply.github.com>
2020-10-20 19:20:10 +0300

    Revert "Merge branch 'main' of https://github.com/Entegrio-tech/Golden-rat into main"
    This reverts commit 6d0866d00a70e53520652fcb79032344015b9426, reversing
    changes made to 3c3fe51c577de40b11b7c8de097d0e24439b213f.

--- Data-base/AutomationAccountingEquipment/CreateTableRequests.sql
+++ Data-base/AutomationAccountingEquipment/CreateTableRequests.sql
@@ -6,6 +6,8 @@ CREATE TABLE Requests
 	EmployeeID SMALLINT REFERENCES Employee (id),
 	EquipmentID SMALLINT REFERENCES Equipment (ID),
 	Text NVARCHAR(500),
-	RequestDate DATE
+	RequestDate DATE,
+	Inventory_number NVARCHAR(30),
+	Components NVARCHAR(1000),
 );
 
