1be60045d62ff1632d6c4a886456c8d15d76566b
manswisskinfe <62439500+manswisskinfe@users.noreply.github.com>
2020-10-24 20:02:50 +0300

    Authentication
    Код к форме авторизации

--- Resourse/AAE/Form1.cs
+++ Resourse/AAE/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace AAE
 {
@@ -15,6 +17,33 @@ namespace AAE
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string sqlExpression = $@"SELECT * FROM Employee
+                                          WHERE Login = '{textBoxLogin.Text}' AND Password = CAST('{textBoxPassword.Text}' AS varbinary)";
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                    labelError.Text = "Успех";
+                else
+                {
+                    reader.Close();
+                    string sqlExpression1 = $@"SELECT * FROM Employee
+                                               WHERE Login = '{textBoxLogin.Text}'";
+                    command.CommandText = sqlExpression1;
+                    reader = command.ExecuteReader();
+                    if (reader.HasRows)
+                        labelError.Text = "Неверный пароль";
+                    else
+                        labelError.Text = "Такого пользователя не существует";
+                }
+            }
         }
     }
 }
